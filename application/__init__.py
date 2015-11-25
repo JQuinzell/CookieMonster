5cf1cbd79316b21239d5c6f83bbc879f97ff2394
Rainwater <jqrvz6@mst.edu>
2015-11-24 20:12:20 -0600

    Send static files

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 from .main import main as main_blueprint
 app.register_blueprint(main_blueprint)
--- application/main/views.py
+++ application/main/views.py
@@ -1,5 +1,11 @@
+# from flask import send_static_file
 from . import main
+from flask import current_app
 
 @main.route('/')
 def index():
-  return "Hello world"
+  return current_app.send_static_file('index.html')
+
+@main.route('/static/<path:path>')
+def asset(path):
+  return current_app.send_static_file(path)
