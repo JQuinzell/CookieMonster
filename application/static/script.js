579f2f35697fc9345dab4f66c2a521e80690f774
Rainwater <jqrvz6@mst.edu>
2015-11-26 02:27:07 -0600

    Setup script

--- application/static/script.js
+++ application/static/script.js
@@ -1,6 +1,51 @@
+var $main
+
 $(function(){
+  $main = $("#main-content")
+
+  loadMainPage()
+
+  $("li.cookies").click(loadCookiePage)
+  $("li.warehouses").click(loadWarehousePage)
+  $("li.distributors").click(loadDistributorPage)
+  $("li.buyers").click(loadBuyerPage)
+})
+
+function setContent(html) {
+  $main.html(html)
+}
+
+function loadMainPage() {
   $.ajax("/static/templates/main.html")
     .done(function(data){
-      $("#main-content").html(data)
-    });
-})
+      setContent(data)
+    })
+}
+
+function loadCookiePage() {
+  $.ajax("/static/templates/cookies.html")
+    .done(function(data){
+      setContent(data)
+    })
+}
+
+function loadWarehousePage() {
+  $.ajax("/static/templates/warehouses.html")
+    .done(function(data){
+      setContent(data)
+    })
+}
+
+function loadDistributorPage() {
+  $.ajax("/static/templates/distributors.html")
+    .done(function(data){
+      setContent(data)
+    })
+}
+
+function loadBuyerPage() {
+  $.ajax("/static/templates/buyers.html")
+    .done(function(data){
+      setContent(data)
+    })
+}
