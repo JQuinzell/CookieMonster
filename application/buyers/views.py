d50a1e64fc4cdde1c1af50f93670ae34c026d66d
Rainwater <jqrvz6@mst.edu>
2015-11-30 20:30:57 -0600

    Show buyer

--- application/buyers/views.py
+++ application/buyers/views.py
@@ -4,3 +4,41 @@ from flask import render_template
 @buyers.route('/buyers')
 def index():
   return render_template('buyers/index.html', buyers=[])
+
+@buyers.route('/buyers/<name>')
+def show(name):
+  buyer = {
+    "name": name,
+    "address": "Test",
+    "orders": [
+      {
+        "id": 1,
+        "total": 0,
+        "cookies": [
+          {
+            "name": "Test",
+            "price": "0.00"
+          },
+          {
+            "name": "Test",
+            "price": "0.00"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "total": 0,
+        "cookies": [
+          {
+            "name": "Test",
+            "price": "0.00"
+          },
+          {
+            "name": "Test",
+            "price": "0.00"
+          }
+        ]
+      }
+    ]
+  }
+  return render_template('buyers/show.html', buyer=buyer)
