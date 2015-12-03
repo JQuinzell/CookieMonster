6badd7328a131347757d733e20358258bb0a8839
Rainwater <jqrvz6@mst.edu>
2015-12-03 12:33:10 -0600

    Remove test data

--- application/static/script.js
+++ application/static/script.js
@@ -1,57 +1,5 @@
 var $main
 
-var cookieData = {
-  "cookies": [
-    {
-      name: "Your mother made it",
-      price: "5.99"
-    },
-    {
-      name: "Chocolate",
-      price: "2.99"
-    }
-  ]
-}
-
-var warehouseData = {
-  "warehouses": [
-    {
-      name: "Warehouse1",
-      address: "A street"
-    },
-    {
-      name: "Warehouse9000",
-      address: "Mars"
-    }
-  ]
-}
-
-var distData = {
-  "distributors": [
-    {
-      name: "Gonzo",
-      address: "A Mansion"
-    },
-    {
-      name: "Superman",
-      address: "Batcave"
-    }
-  ]
-}
-
-var buyerData = {
-  "buyers": [
-    {
-      name: "John Locke",
-      address: "Deserted island"
-    },
-    {
-      name: "Yo Mama",
-      address: "Yo house"
-    }
-  ]
-}
-
 $(function(){
   $main = $("#main-content")
 
