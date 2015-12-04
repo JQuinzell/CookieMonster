cdcfebda3c10a78059a2c1a0a509985b7c62f812
Rainwater <jqrvz6@mst.edu>
2015-12-03 22:15:43 -0600

    Accept put request

--- application/buyers/views.py
+++ application/buyers/views.py
@@ -1,5 +1,5 @@
 from . import buyers
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
 
 @buyers.route('/buyers')
 def index():
@@ -16,40 +16,46 @@ def index():
 
   return render_template('buyers/index.html', buyers=buyers)
 
-@buyers.route('/buyers/<name>')
+@buyers.route('/buyers/<name>', methods=['GET', 'PUT'])
 def show(name):
-  buyer = {
-    "name": name,
-    "address": "Test",
-    "orders": [
-      {
-        "id": 1,
-        "total": 0,
-        "cookies": [
-          {
-            "name": "Test",
-            "price": "0.00"
-          },
-          {
-            "name": "Test",
-            "price": "0.00"
-          }
-        ]
-      },
-      {
-        "id": 2,
-        "total": 0,
-        "cookies": [
-          {
-            "name": "Test",
-            "price": "0.00"
-          },
-          {
-            "name": "Test",
-            "price": "0.00"
-          }
-        ]
-      }
-    ]
-  }
-  return jsonify(**buyer)
+  if request.method == 'GET':
+    buyer = {
+      "name": name,
+      "address": "Test",
+      "orders": [
+        {
+          "id": 1,
+          "total": 0,
+          "cookies": [
+            {
+              "name": "Test",
+              "price": "0.00"
+            },
+            {
+              "name": "Test",
+              "price": "0.00"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "total": 0,
+          "cookies": [
+            {
+              "name": "Test",
+              "price": "0.00"
+            },
+            {
+              "name": "Test",
+              "price": "0.00"
+            }
+          ]
+        }
+      ]
+    }
+    return jsonify(**buyer)
+
+  if request.method == 'PUT':
+    # Update buyer
+    update = request.get_json()
+    return jsonify(**update)
